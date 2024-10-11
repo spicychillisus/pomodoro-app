8756d9bcbe2a528e12c259857c39caeb55716b64
spicychillisus <asherben90@gmail.com>
2024-10-12 02:40:15 +0800

    Refactor timer navigation buttons and set Pomodoro time

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,11 +1,21 @@
 
 const minute = document.getElementById('minute');
 const seconds = document.getElementById('seconds');
-minute.innerHTML = 25;
-seconds.innerHTML = "00";
 
 let studyTime = 25 * 60;
+let longBreak = 15 * 60;
+let shortBreak = 5 * 60;
+
+let studyTimeMin = 25;
+let longBreakMin = 15;
+let shortBreakMin = 5;
+
+seconds.innerHTML = '00';
 
 function startTimer() {
     console.log("you suck")
+}
+
+function setPomodoroTime() {
+    minute.innerHTML = studyTimeMin;
 }
